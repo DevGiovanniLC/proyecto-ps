3227b860d4fb9c386ba31d86df582feaedd02f0a
Giovanni León Corujo <giovannilc.dev@gmail.com>
2024-03-11 20:11:45 +0000

    single responsabilty principle done

---
+++ src/app/record-button/VideoRecorder.ts
@@ -0,0 +1,76 @@
+
+
+export class VideoRecorder{
+    mediaRecorder: any = null;
+
+	async start(framerate_value:number, resolution_value:number): Promise<void> {
+		let media = await this.getMedia(
+			framerate_value,
+			resolution_value
+		);
+
+		this.mediaRecorder = this.getMediaRecorder(media);
+
+		if (this.mediaRecorder) {
+			(this.mediaRecorder as MediaRecorder).onstop = (event) => {
+				this.mediaRecorder = null;
+			};
+		}
+
+		this.mediaRecorder.start();
+
+		const video = this.getVideoTrack(media);
+
+		video.addEventListener('ended', async () => {
+			this.mediaRecorder.stop();
+		});
+
+		this.mediaRecorder.addEventListener(
+			'dataavailable',
+			(event: BlobEvent) => {
+				this.downloadVideo(event);
+			}
+		);
+	}
+
+	private async getMedia(
+		framerate: number,
+		resolution: number
+	): Promise<MediaStream> {
+		return navigator.mediaDevices.getDisplayMedia({
+			video: {
+				frameRate: { ideal: framerate, max: 60 },
+				height: resolution,
+			},
+			audio: true,
+		});
+	}
+
+	private getMediaRecorder(media: MediaStream): MediaRecorder {
+		return new MediaRecorder(media, {
+			mimeType: 'video/x-matroska',
+		});
+	}
+
+
+	private getVideoTrack(media: MediaStream): MediaStreamTrack {
+		return media.getVideoTracks()[0];
+	}
+
+	private async downloadVideo(event: BlobEvent): Promise<void> {
+		const link = document.createElement('a');
+		link.href = URL.createObjectURL(event.data)
+		link.download = 'captura.mkv';
+		link.click();
+	}
+
+
+    public isRecording(): boolean{
+        return this.mediaRecorder != null
+    }
+
+    public stop(){
+        this.mediaRecorder.stop();
+        this.mediaRecorder = null;
+    }
+}
--- src/app/record-button/record-button.component.ts
+++ src/app/record-button/record-button.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { VideoConverter } from './videoConverter';
+import { VideoRecorder } from './VideoRecorder';
 
 @Component({
 	selector: 'app-record-button',
@@ -9,98 +9,26 @@ import { VideoConverter } from './videoConverter';
 	templateUrl: './record-button.component.html',
 	styleUrl: './record-button.component.css',
 })
+
 export class RecordButtonComponent {
-	mediaRecorder: any = null;
 	framerate_value: number = 60;
 	resolution_value: number = 1080;
-	videoConverter = new VideoConverter()
+	videoRecorder =  new VideoRecorder()
 
 	ngAfterViewInit(): void {
 		const record_button = document.getElementById('record-button');
 
 		if (record_button) {
 			record_button.addEventListener('click', (event) => {
-				if (this.mediaRecorder == null) {
-					this.setRecord(event);
-					return;
-				}
-
-				if (this.mediaRecorder.state == 'recording') {
-					this.mediaRecorder.stop();
-					this.mediaRecorder = null;
+				if (this.videoRecorder.isRecording()) {
+					this.videoRecorder.stop()
+				}else{
+					this.videoRecorder.start(this.framerate_value, this.resolution_value)
 				}
 			});
 		}
-
-		this.getCodecs();
 	}
 
-	async setRecord(event: MouseEvent): Promise<void> {
-		let media = await this.getMedia(
-			this.framerate_value,
-			this.resolution_value
-		);
 
-		this.mediaRecorder = this.getMediaRecorder(media);
-
-		if (this.mediaRecorder) {
-			(this.mediaRecorder as MediaRecorder).onstop = (event) => {
-				this.mediaRecorder = null;
-			};
-		}
-
-		this.mediaRecorder.start();
-
-		const video = this.getVideoTrack(media);
-
-		video.addEventListener('ended', async () => {
-			this.mediaRecorder.stop();
-		});
-
-		this.mediaRecorder.addEventListener(
-			'dataavailable',
-			(event: BlobEvent) => {
-				this.downloadVideo(event);
-			}
-		);
-	}
-
-	private async getMedia(
-		framerate: number,
-		resolution: number
-	): Promise<MediaStream> {
-		return navigator.mediaDevices.getDisplayMedia({
-			video: {
-				frameRate: { ideal: framerate, max: 60 }, // standar framerate 15, 24, 30, 60
-				height: resolution,
-			},
-			audio: true,
-		});
-	}
-
-	private getMediaRecorder(media: MediaStream): MediaRecorder {
-		return new MediaRecorder(media, {
-			mimeType: 'video/x-matroska',
-		});
-	}
-
-	private getCodecs(): void {
-		const video = RTCRtpReceiver.getCapabilities('video')?.codecs;
-		const audio = RTCRtpReceiver.getCapabilities('audio')?.codecs;
-		video?.forEach((codec) => {
-			console.log(codec.mimeType);
-		});
-	}
-
-	private getVideoTrack(media: MediaStream): MediaStreamTrack {
-		return media.getVideoTracks()[0];
-	}
-
-	private async downloadVideo(event: BlobEvent): Promise<void> {
-		const link = document.createElement('a');
-		link.href = await this.videoConverter.transcode(URL.createObjectURL(event.data))
-		link.download = 'captura.mkv';
-		link.click();
-	}
 
 }
