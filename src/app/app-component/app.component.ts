8c828f64c3d62b284d6d81ab26575fcdfe9f66a7
Daniel <114070762+danielhdezschez@users.noreply.github.com>
2024-03-15 10:49:57 +0000

    Primera Prueba Merge

--- src/app/app-component/app.component.ts
+++ src/app/app-component/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RecordButtonComponent } from '../record-button/record-button.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, RecordButtonComponent],
+	imports: [RouterOutlet, RecordButtonComponent, HeaderComponent],
 	templateUrl: './app.component.html',
 })
 export class AppComponent {}
