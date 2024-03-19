da7a6df48fe2bda8f64761565fd7aabdd5447c85
Giovanni León Corujo <giovannilc.dev@gmail.com>
2024-03-19 17:08:32 +0000

    Components folder created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,35 @@
 import { Component, ViewChild } from '@angular/core';
-import { RouterLink, RouterOutlet,NavigationEnd,Router } from '@angular/router';
-import { RecordButtonComponent } from './record-button/record-button.component';
-import { HeaderComponent } from './header/header.component';
+import {
+	RouterLink,
+	RouterOutlet,
+	NavigationEnd,
+	Router,
+} from '@angular/router';
+import { RecordButtonComponent } from './components/record-button/record-button.component';
+import { HeaderComponent } from './components/header/header.component';
 import { WhoweareComponent } from './whoweare/whoweare.component';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet,RouterLink, RecordButtonComponent, HeaderComponent,WhoweareComponent],
+	imports: [
+		RouterOutlet,
+		RouterLink,
+		RecordButtonComponent,
+		HeaderComponent,
+		WhoweareComponent,
+	],
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
 	showLandingPage: boolean = true;
 	constructor(private router: Router) {
 		this.router.events.subscribe((event) => {
-		  if (event instanceof NavigationEnd) {
-			// Si la ruta es diferente a la ruta de la landing page, ocultarla
-			this.showLandingPage = (event.url !== '/');
-		  }
+			if (event instanceof NavigationEnd) {
+				// Si la ruta es diferente a la ruta de la landing page, ocultarla
+				this.showLandingPage = event.url !== '/';
+			}
 		});
 	}
 }
