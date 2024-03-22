e6cc3f9c5ae7b1f3cb0778b845743b2cafcfff75
Giovanni <giovannilc.dev@gmail.com>
2024-03-22 10:51:43 +0000

    screenshot button refactored

--- src/app/components/screenshot-button/screenshot-button.component.ts
+++ src/app/components/screenshot-button/screenshot-button.component.ts
@@ -9,18 +9,14 @@ import { ScreenshotTaker } from './ScreenshotTaker';
 	templateUrl: './screenshot-button.component.html',
 	styleUrl: './screenshot-button.component.css',
 })
-export class ScreenshotButtonComponent implements AfterViewInit {
-	screenshotTaker = new ScreenshotTaker();
-	@ViewChild('screenshot_button') screenshot_button!: ElementRef;
+export class ScreenshotButtonComponent {
+	screenshotTaker: ScreenshotTaker
 
-	ngAfterViewInit(): void {
-		if (this.screenshot_button) {
-			this.screenshot_button.nativeElement.addEventListener(
-				'click',
-				(event: Event) => {
-					this.screenshotTaker.take();
-				}
-			);
-		}
+	constructor(){
+		this.screenshotTaker = new ScreenshotTaker()
+	}
+
+	screenshotEvent(): void {
+		this.screenshotTaker.take();
 	}
 }
