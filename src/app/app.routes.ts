d2221bacd1b55d30d0761e69d350154010617e7c
augusto <augustopaolone3@hotmail.com>
2024-04-06 18:08:33 +0200

    añadi la base de signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 
 import { WhoweareComponent } from './whoweare-page/whoweare.component';
+import { LogInComponent } from "./components/log-in/log-in.component";
+import { SignUpComponent } from "./components/sign-up/sign-up.component";
 
 export const routes: Routes = [
 	{ path: 'whoweare', component: WhoweareComponent },
+
+  {path:"login",component:LogInComponent},
+
+  {path:"signup",component:SignUpComponent},
+
 ];
