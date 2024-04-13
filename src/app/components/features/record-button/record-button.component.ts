63ce8a83852cbcf1215ed1b174c49655f4d98cf0
Giovanni León Corujo <giovannilc.dev@gmail.com>
2024-04-13 15:53:13 +0100

    CounterDown and record state created

--- src/app/components/features/record-button/record-button.component.ts
+++ src/app/components/features/record-button/record-button.component.ts
@@ -4,6 +4,7 @@ import { VideoRecorder } from './VideoRecorder';
 import { ScreenshotButtonComponent } from '../screenshot-button/screenshot-button.component';
 import { OptionsComponent } from '../options/options.component';
 import { NextObserver } from 'rxjs';
+import { CounterDown } from './CounterDown';
 
 @Component({
 	selector: 'app-record-button',
@@ -13,15 +14,20 @@ import { NextObserver } from 'rxjs';
 	styleUrl: './record-button.component.css',
 })
 export class RecordButtonComponent implements NextObserver<any> {
-	@ViewChild('record_button') record_button!: ElementRef;
-	@ViewChild('micro_button') micro_button!: ElementRef;
+	@ViewChild('record_button') record_button: ElementRef;
+	@ViewChild('micro_button') micro_button: ElementRef;
+
 	videoRecorder: VideoRecorder;
+
 	microState: boolean;
+	recordState: string;
+
 	@Input() framerate: number;
 	@Input() resolution: number;
 	@Input() delay: number;
 
 	constructor() {
+		this.recordState = 'RECORD';
 		this.microState = true;
 	}
 
@@ -35,8 +41,7 @@ export class RecordButtonComponent implements NextObserver<any> {
 			this.videoRecorder.stop();
 		} else {
 			this.videoRecorder.microphone(this.microState);
-
-			await this.videoRecorder.start(
+			this.videoRecorder.start(
 				this.framerate,
 				this.resolution,
 				this.delay
@@ -44,20 +49,36 @@ export class RecordButtonComponent implements NextObserver<any> {
 		}
 	}
 
-	next(mediaRecorder: MediaRecorder): void {
-		if (mediaRecorder == null) return;
+	next(data: any): void {
+		if (typeof data === 'number') {
+			this.countDownEvents(data);
+			return;
+		}
 
+		if (data instanceof MediaRecorder) {
+			this.mediaRecorderEvents(data);
+			return;
+		}
+	}
+
+	private mediaRecorderEvents(mediaRecorder: MediaRecorder) {
 		mediaRecorder.addEventListener('start', async () => {
 			this.record_button.nativeElement.style.backgroundImage =
 				"url('/assets/recording_state.png')";
 			this.micro_button.nativeElement.disabled = true;
+			this.recordState = 'RECORDING';
 		});
 
 		mediaRecorder.addEventListener('dataavailable', () => {
 			this.record_button.nativeElement.style.backgroundImage =
 				"url('/assets/stopped_state.png')";
 			this.micro_button.nativeElement.disabled = false;
+			this.recordState = 'RECORD';
 		});
+	}
+
+	private countDownEvents(second: number) {
+		this.recordState = second.toString();
 	}
 
 	toggleMicrophone() {
