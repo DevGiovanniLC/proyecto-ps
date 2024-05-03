227862d0ee7b31e29cd4dc42daf26b90231fd287
Giovanni León Corujo <giovannilc.dev@gmail.com>
2024-05-03 20:49:48 +0100

    previsualization refactored

--- src/app/components/features/previsualition-content-dialog/previsualition-content-dialog.component.ts
+++ src/app/components/features/previsualition-content-dialog/previsualition-content-dialog.component.ts
@@ -1,28 +1,27 @@
 import { Component, Inject } from "@angular/core";
-import {MAT_DIALOG_DATA, MatDialogClose, MatDialogContent} from "@angular/material/dialog";
+import { MAT_DIALOG_DATA, MatDialogClose, MatDialogContent } from "@angular/material/dialog";
 import { HTTPVideoTransfer } from "../../../services/HTTPVideoTransfer.service";
+import { strict } from "assert";
 
 @Component({
-  selector: 'app-previsualition-content-dialog',
-  standalone: true,
-  providers: [HTTPVideoTransfer],
-  imports: [
-    MatDialogContent,
-    MatDialogClose
-  ],
-  templateUrl: './previsualition-content-dialog.component.html',
-  styleUrl: './previsualition-content-dialog.component.css'
+    selector: 'app-previsualition-content-dialog',
+    standalone: true,
+    providers: [HTTPVideoTransfer],
+    imports: [
+        MatDialogContent,
+        MatDialogClose
+    ],
+    templateUrl: './previsualition-content-dialog.component.html',
+    styleUrl: './previsualition-content-dialog.component.css'
 })
 export class PrevisualitionContentDialogComponent {
-  VideoUrl : string
-  blob: Blob
-  format: string;
-  constructor( private httpVideo: HTTPVideoTransfer, @Inject(MAT_DIALOG_DATA) public data:any) {
-    this.blob = data.blobData
-    this.VideoUrl = URL.createObjectURL(data.blobData);
-    this.format = data.format
-  }
-  async downloadVideo(data: Blob): Promise<void> {
-    this.httpVideo.sendVideo(data, this.format)
-  }
+    VideoUrl: string
+
+    constructor(private httpVideo: HTTPVideoTransfer, @Inject(MAT_DIALOG_DATA) public data) {
+        this.VideoUrl = URL.createObjectURL(this.data.blobData)
+    }
+
+    async downloadVideo(): Promise<void> {
+        this.httpVideo.sendVideo(this.data.blobData, this.data.format)
+    }
 }
