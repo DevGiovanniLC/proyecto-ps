8480bfd8ef3830bb33d278022cfefbbf435122c2
Giovanni León Corujo <giovannilc.dev@gmail.com>
2024-05-05 00:55:46 +0100

    atomatic browser format fixed

--- src/app/services/HTTPVideoTransfer.service.ts
+++ src/app/services/HTTPVideoTransfer.service.ts
@@ -20,19 +20,19 @@ export class HTTPVideoTransfer {
             .then(async (res) => {
                 res.arrayBuffer().then((data) => {
                     if (func) func();
-                    this.downloadVideo(new Blob([data]), format)
+                    this.downloadVideo(new Blob([data]))
                 })
             })
             .catch(error => {
-                this.downloadVideo(file, "mkv")
+                this.downloadVideo(file)
             });
 
     }
 
-    private async downloadVideo(data: Blob, format: string): Promise<void> {
+    private async downloadVideo(data: Blob): Promise<void> {
         const link = document.createElement('a');
         link.href = URL.createObjectURL(data);
-        link.download = 'video.' + format;
+        link.download = 'video'
         link.click();
     }
 
