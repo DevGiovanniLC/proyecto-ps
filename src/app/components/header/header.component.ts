f8b368d108b83fa8ed4a11a1945f7c98f4397d47
Daniel <114070762+danielhdezschez@users.noreply.github.com>
2024-05-13 13:24:39 +0100

    Translation implemented on Menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,26 +1,56 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { LogInComponent } from "../auth/log-in/log-in.component";
 import firebase from "firebase/compat";
 import { AuthService } from "../../services/AuthService.service";
-
-
+import { TranslationService } from "../../../translation";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [RouterLink, LogInComponent],
+    imports: [RouterLink, LogInComponent, FormsModule, CommonModule],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
     isloogedIn: boolean
     firebaseService = inject(AuthService);
-
-    constructor() {
-
+    targetLanguages = ['es', 'ar', 'fr', 'it', 'en', 'pt', 'zh-CN', 'ja', 'ru', 'hi'];
+    menuItems: string[] = ['WHO WE ARE', 'LOG IN', 'SIGN UP', 'LOG OUT'];
+    selectedLanguage: string = 'en';
+    jsonData: any;
+    constructor(private traduccion: TranslationService, private http: HttpClient) {
+    
 
     }
+
+    ngOnInit() {
+        this.http.get<any>("../../../assets/i18n/header_content.json").subscribe(data => {
+            this.jsonData = data;
+        });
+    }
+
+    translateAll() {
+        
+
+        let values: string[] = Object.values(this.jsonData);
+        
+        values.forEach((text, index) => {
+          this.translate(text, index);
+        });
+      }
+
+    translate(text: string, index: number) {
+        this.traduccion.translateText(text, this.selectedLanguage)
+          .subscribe((response: any) => {
+            this.menuItems[index] = response.data.translations[0].translatedText;
+          }, (error) => {
+            console.error('Error al traducir:', error);
+          });
+      }
 
     storage() {
 
