71021e9d5db1e34db3bacfb04d4f1287d171c155
Giovanni León Corujo <giovannilc.dev@gmail.com>
2024-05-16 13:55:40 +0100

    Options in record button

--- src/app/components/features/options/options.component.ts
+++ src/app/components/features/options/options.component.ts
@@ -5,62 +5,40 @@ import { AuthService } from "../../../services/AuthService.service";
 
 
 @Component({
-	selector: 'app-options',
-	standalone: true,
-	imports: [FormsModule, ScreenshotButtonComponent],
-	templateUrl: './options.component.html',
-	styleUrl: './options.component.css',
+    selector: 'app-options',
+    standalone: true,
+    imports: [FormsModule, ScreenshotButtonComponent],
+    templateUrl: './options.component.html',
+    styleUrl: './options.component.css',
 })
 export class OptionsComponent {
-	framerate: number;
-	resolution: number;
-	delay: number;
-	videoformat: string;
-	imageformat: string
+    framerate: number;
+    resolution: number;
+    delay: number;
+    videoformat: string;
+    imageformat: string
 
-	@Output() framerate_event: EventEmitter<number> = new EventEmitter<number>();
-	@Output() resolution_event: EventEmitter<number> = new EventEmitter<number>();
-	@Output() delay_event: EventEmitter<number> = new EventEmitter<number>();
-	@Output() formatvideochange: EventEmitter<string> = new EventEmitter<string>();
-	@Output() formatimagechange: EventEmitter<string> = new EventEmitter<string>();
+    constructor() {
+        this.framerate = 60;
+        this.resolution = 1080;
+        this.videoformat = "mp4";
+        this.imageformat = "png";
+        this.delay = 0;
+    }
 
-	constructor() {
-		this.framerate = 60;
-		this.resolution = 1080;
-		this.videoformat = "mp4";
-		this.imageformat = "png";
-		this.delay = 0;
-	}
+    firebaseService = inject(AuthService);
 
-	firebaseService = inject(AuthService);
+    storage() {
+        return (localStorage.getItem('isloogedIn') === 'true');
+    }
 
-	storage() {
-		return (localStorage.getItem('isloogedIn') === 'true');
-	}
-
-	onFramerateChange() {
-		this.framerate_event.emit(this.framerate);
-	}
-	onResolutionChange() {
-		this.resolution_event.emit(this.resolution);
-	}
-	onDelayChange() {
-		this.delay_event.emit(this.delay);
-	}
-	onformatVideoChange() {
-		this.formatvideochange.emit(this.videoformat);
-
-	}
-	onformatImageChange() {
-		this.formatimagechange.emit(this.imageformat);
-	}
-  getOptions() {
-    return {
-      framerate: this.framerate,
-      resolution: this.resolution,
-      delay: this.delay,
-      videoformat: this.videoformat,
-      imageformat: this.imageformat
-    };
-  }
+    getOptions() {
+        return {
+            framerate: this.framerate,
+            resolution: this.resolution,
+            delay: this.delay,
+            videoformat: this.videoformat,
+            imageformat: this.imageformat
+        };
+    }
 }
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,44 +1,41 @@
-import { Component, OnInit, EventEmitter,Output} from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { RecordButtonComponent } from '../components/features/record-button/record-button.component';
-import HeaderComponent  from '../components/header/header.component';
+import HeaderComponent from '../components/header/header.component';
 import { OptionsComponent } from '../components/features/options/options.component';
 import { ScreenshotButtonComponent } from '../components/features/screenshot-button/screenshot-button.component';
 import { PrevisualitionContentDialogComponent } from '../components/features/previsualition-content-dialog/previsualition-content-dialog.component';
 
 @Component({
-  selector: 'app-home-page',
-  standalone: true,
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css'],
-  imports: [
-    RouterOutlet,
-    RouterLink,
-    RecordButtonComponent,
-    HeaderComponent,
-    OptionsComponent,
-    ScreenshotButtonComponent,
-    PrevisualitionContentDialogComponent
-]
+    selector: 'app-home-page',
+    standalone: true,
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.css'],
+    imports: [
+        RouterOutlet,
+        RouterLink,
+        RecordButtonComponent,
+        HeaderComponent,
+        OptionsComponent,
+        ScreenshotButtonComponent,
+        PrevisualitionContentDialogComponent
+    ]
 })
 export class HomePageComponent implements OnInit {
-  options: any = {
-    framerate: 60, // Valores predeterminados
-    resolution: 1080,
-    delay: 0,
-    videoformat: 'mp4',
-    imageformat: 'png'
-  };
+    options: any = {
+        framerate: 60, // Valores predeterminados
+        resolution: 1080,
+        delay: 0,
+        videoformat: 'mp4',
+        imageformat: 'png'
+    };
+    constructor() { }
 
+    ngOnInit() {
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  receiveOptions(result: any) {
-    this.options = result;
-    console.log('Opciones recibidas en el MainComponent:', this.options);
-
-  }
+    receiveOptions(result: any) {
+        this.options = result;
+        console.log('Opciones recibidas en el MainComponent:', this.options);
+    }
 }
