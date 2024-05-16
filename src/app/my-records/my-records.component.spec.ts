b4cdcfd1ccd9c6728e3a43ada070bf6ad1799e3e
Roichu5k <roimontero2003@gmail.com>
2024-05-16 23:26:22 +0100

    Mejorado el código typescript de my records

--- src/app/my-records/my-records.component.spec.ts
+++ src/app/my-records/my-records.component.spec.ts
@@ -676,6 +676,346 @@ describe('MyRecordsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyRecordsComponent } from './my-records.component';
+
+describe('MyRecordsComponent', () => {
+  let component: MyRecordsComponent;
+  let fixture: ComponentFixture<MyRecordsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyRecordsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyRecordsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyRecordsComponent } from './my-records.component';
+
+describe('MyRecordsComponent', () => {
+  let component: MyRecordsComponent;
+  let fixture: ComponentFixture<MyRecordsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyRecordsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyRecordsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyRecordsComponent } from './my-records.component';
+
+describe('MyRecordsComponent', () => {
+  let component: MyRecordsComponent;
+  let fixture: ComponentFixture<MyRecordsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyRecordsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyRecordsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyRecordsComponent } from './my-records.component';
+
+describe('MyRecordsComponent', () => {
+  let component: MyRecordsComponent;
+  let fixture: ComponentFixture<MyRecordsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyRecordsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyRecordsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyRecordsComponent } from './my-records.component';
+
+describe('MyRecordsComponent', () => {
+  let component: MyRecordsComponent;
+  let fixture: ComponentFixture<MyRecordsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyRecordsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyRecordsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyRecordsComponent } from './my-records.component';
+
+describe('MyRecordsComponent', () => {
+  let component: MyRecordsComponent;
+  let fixture: ComponentFixture<MyRecordsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyRecordsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyRecordsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyRecordsComponent } from './my-records.component';
+
+describe('MyRecordsComponent', () => {
+  let component: MyRecordsComponent;
+  let fixture: ComponentFixture<MyRecordsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyRecordsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyRecordsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyRecordsComponent } from './my-records.component';
+
+describe('MyRecordsComponent', () => {
+  let component: MyRecordsComponent;
+  let fixture: ComponentFixture<MyRecordsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyRecordsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyRecordsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+
+
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyRecordsComponent } from './my-records.component';
+
+describe('MyRecordsComponent', () => {
+  let component: MyRecordsComponent;
+  let fixture: ComponentFixture<MyRecordsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyRecordsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyRecordsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyRecordsComponent } from './my-records.component';
+
+describe('MyRecordsComponent', () => {
+  let component: MyRecordsComponent;
+  let fixture: ComponentFixture<MyRecordsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyRecordsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyRecordsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyRecordsComponent } from './my-records.component';
+
+describe('MyRecordsComponent', () => {
+  let component: MyRecordsComponent;
+  let fixture: ComponentFixture<MyRecordsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyRecordsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyRecordsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyRecordsComponent } from './my-records.component';
+
+describe('MyRecordsComponent', () => {
+  let component: MyRecordsComponent;
+  let fixture: ComponentFixture<MyRecordsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyRecordsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyRecordsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyRecordsComponent } from './my-records.component';
+
+describe('MyRecordsComponent', () => {
+  let component: MyRecordsComponent;
+  let fixture: ComponentFixture<MyRecordsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyRecordsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyRecordsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyRecordsComponent } from './my-records.component';
+
+describe('MyRecordsComponent', () => {
+  let component: MyRecordsComponent;
+  let fixture: ComponentFixture<MyRecordsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyRecordsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyRecordsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+
 });
 
 
